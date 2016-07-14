d0be5bf16031ea684005b361d481b5bce99eff84
igorcamposrocha <igorcamposrocha@gmail.com>
2016-07-13 21:29:25 -0300

    Criado

---
+++ app/src/main/java/rocha/igor/jogo_da_velha/MainActivity.java
@@ -0,0 +1,13 @@
+package rocha.igor.jogo_da_velha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);a
+    }
+}
